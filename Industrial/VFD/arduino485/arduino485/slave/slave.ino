467b5cee667db3685fda152e31f731699c638510
dusica511 <155007364+dusica511@users.noreply.github.com>
2024-06-21 11:37:22 +0200

    Update slave.ino

--- Industrial/VFD/arduino485/arduino485/slave/slave.ino
+++ Industrial/VFD/arduino485/arduino485/slave/slave.ino
@@ -11,17 +11,18 @@ char receivedChar;
 
 void setup() {
   // put your setup code here, to run once:
-Serial.begin(9600);
-RS_Slave.begin(9600);
-pinMode(RS_DE_RE, OUTPUT);
-//since this slave mode "Always recieve", the enable pin need to be low
-digitalWrite(RS_DE_RE, LOW);
-Serial.println("The Slave node is ready...");
+  Serial.begin(9600);
+  RS_Slave.begin(9600);
+  pinMode(RS_DE_RE, OUTPUT);
+  //since this slave mode "Always recieve", the enable pin need to be low
+  digitalWrite(RS_DE_RE, LOW);
+  Serial.println("The Slave node is ready...");
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  if (RS_Slave.available())
-  Serial.write(RS_Slave.read());
+  if (RS_Slave.available()){
+    Serial.write(RS_Slave.read());
+  }
  
 }
